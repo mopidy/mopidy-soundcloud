c20ef7e574e75e396c258258f093ad1d7ac1194b
Stein Magnus Jodal <stein.magnus@jodal.no>
2026-09-12 17:49:49 +0200

    chore: Update to mopidy-ext-template v2.4.0

--- src/mopidy_soundcloud/__init__.py
+++ src/mopidy_soundcloud/__init__.py
@@ -12,10 +12,10 @@ class Extension(ext.Extension):
     ext_name = "soundcloud"
     version = __version__
 
-    def get_default_config(self):
+    def get_default_config(self) -> str:
         return config.read(pathlib.Path(__file__).parent / "ext.conf")
 
-    def get_config_schema(self):
+    def get_config_schema(self) -> config.ConfigSchema:
         schema = super().get_config_schema()
         schema["explore_songs"] = config.Integer(optional=True)
         schema["auth_token"] = config.Secret()
@@ -34,7 +34,7 @@ class Extension(ext.Extension):
             )
             raise ExtensionError(msg)
 
-    def setup(self, registry):
+    def setup(self, registry: ext.Registry) -> None:
         from .actor import SoundCloudBackend  # noqa: PLC0415
 
         registry.add("backend", SoundCloudBackend)
